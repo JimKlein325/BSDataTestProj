a1e0eec95049e920c31af19c9dc8da9cc5aadc95
Jim Klein <j.mesklein@gmail.com>
2016-09-25 17:47:19 -0700

    initial bird manager code.

--- src/EFBirdData/Models/Bird.cs
+++ src/EFBirdData/Models/Bird.cs
@@ -13,14 +13,18 @@ namespace EFBirdData.Models
         [Key]
         public int Id { get; set; }
         public string CommonName { get; set; }
-        public int PrimaryColorId { get; set; }
-        public PrimaryColor PrimaryColor { get; set; }
-        public int SecondaryColorId { get; set; }
-        public SecondaryColor SecondaryColor { get; set; }
+        public string Family { get; set; }
+        public string ScientificName { get; set; }
+        public string PrimaryColor { get; set; }
+        public string SecondaryColor { get; set; }
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public double Weight { get; set; }
+        public string Size { get; set; }
+        public string ConservationStatus { get; set; }
+        public string ConservationCode { get; set; }
         public ICollection<BirdsPlaces> Habitats { get; set; }
         public ICollection<Sighting> Sightings { get; set; }
         public ICollection<BirdsTernaryColors> BirdsTernaryColors { get; set;}
-
-
     }
 }
---
+++ src/EFBirdData/Models/BirdManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFBirdData.Models;
+
+namespace EFBirdData.Models
+{
+    public static class BirdManager
+    {
+        public static string[] Colors;
+        public static Bird BuildModelBirdFromRepo(BirdWatcher.Bird birdIn)
+        {
+            var newBird = new EFBirdData.Models.Bird()
+            {
+                CommonName = birdIn.CommonName,
+                Family = birdIn.Family,
+                ScientificName = birdIn.ScientificName,
+                PrimaryColor = birdIn.PrimaryColor,
+                SecondaryColor = birdIn.SecondaryColor,
+                Length = birdIn.Length,
+                Width = birdIn.Width,
+                Weight = birdIn.Weight,
+                Size = birdIn.Size,
+                ConservationStatus = birdIn.ConservationStatus,
+                ConservationCode = birdIn.ConservationCode
+            };
+            return newBird;
+        }
+    }
+}
